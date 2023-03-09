4267cf804b3c2f36908c09b6559477ca366ab352
Pat Bills <billspat@users.noreply.github.com>
2023-03-09 09:31:30 -0500

    added output types, net type from env or flex

--- docker/gpapi.py
+++ docker/gpapi.py
@@ -12,10 +12,11 @@ import pathlib
 
 import pandas as pd
 
+from typing import Union
 from pydantic import BaseModel, Field
 from geneplexus import GenePlexus, config, util
 
-##### types
+############ types
 GENESET = list[str]
 
 sample_geneset = [
@@ -25,15 +26,17 @@ sample_geneset = [
 
 class GPInput(BaseModel):
     """# model for input params to a GP model.  
-    net_type can be overridden, but by default NETWORK env used by Docker GP
+    net_type is optional so a pre-set value can be used can be overridden
+    When using inside network-specific docker image, don't set net_type when creating types
     """
-    net_type: config.NET_TYPE = os.getenv('NETWORK')    
+    # allow net_type to be empty, and if so use a pre-set net type when running the pipeline
+    net_type: Union[config.NET_TYPE, None] = None   
     features: config.FEATURE_TYPE 
     gsc: config.GSC_TYPE 
     geneset: GENESET = sample_geneset
-    
-  
-class GPProb(BaseModel):
+
+class GPprob(BaseModel):
+    " also use for node types"
     Entrez: int
     Symbol: str
     Name: str
@@ -42,19 +45,38 @@ class GPProb(BaseModel):
     Class_Label: str = Field(alias="Class-Label")
     Rank: int
 
- 
-class GPOutput(BaseModel):
-    probs: list[GPProb]
-    # sim_GO: pd.DataFrame
-    # convert_out_subset: pd.DataFrame
+class GPedge(BaseModel):
+    Node1:int
+    Node2:int
+    Weight:int
     
-    # class Config:
-    #     arbitrary_types_allowed = True
+    
+class GPsimGo(BaseModel):
+    # example data: 'GO:0070202', 'regulation of establishment of protein localization to chromosome', '5.021538922627177', '1'      
+    ID:str
+    Name:str
+    Similarity:float
+    Rank:int
 
+class GPOutput(BaseModel):
+    probs: list[GPprob]
+    sim_go: list[GPsimGo]
+    edge_list: list[GPedge]
+    # sim_dis, 
+    # avgps, 
+    # edgelist, 
+    # convert_out, 
+    # positive_genes,
+    # graph?
+    class Config:
+         arbitrary_types_allowed = True
+
+
+####### functionality  
 class GPRunner():
-    
+    """ class to run GP, collect outputs and create graph structure"""
     @classmethod
-    def all_data_files_present(cls,file_loc):
+    def all_data_files_present(cls,file_loc:str)->bool:
         """validate presence of data files in folder"""
     
         if not os.path.exists(file_loc):
@@ -66,38 +88,53 @@ class GPRunner():
     
         return True
     
-    
-    def __init__(self,file_loc:str):
+    def __init__(self,file_loc:str, net_type:config.NET_TYPE):
         
         # validate backend data
         if not(GPRunner.all_data_files_present(file_loc)):
             raise Exception(f"backend data path not found or not complete in {file_loc}")
         
         self.file_loc = file_loc
+        self.net_type = net_type
         
     def run(self,gpinput:GPInput) -> GPOutput:
-        gp = GenePlexus(self.file_loc, gpinput.net_type, gpinput.features, gpinput.gsc)
+        # if no net_type is sent, use class property
+        gp = GenePlexus(self.file_loc, 
+                        gpinput.net_type or self.net_type, 
+                        gpinput.features, 
+                        gpinput.gsc
+                        )
+        
+        # load genes on separate process for profiling and debugging
         gp.load_genes(gpinput.geneset)
         mdl_weights, df_probs, avgps = gp.fit_and_predict()
-        
-        # TODO these other steps are disabled until type model is added for the outputs
-        # df_sim_GO, df_sim_Dis, weights_GO, weights_Dis = gp.make_sim_dfs()
-        # # Return an edgelist
-        # df_edge, isolated_genes, df_edge_sym, isolated_genes_sym = gp.make_small_edgelist(num_nodes=50)
-        # # Return the validation datframwe for just the network that was used in the pipeline
-        # df_convert_out_subset, positive_genes = gp.alter_validation_df()
-        
-        # for this first test, only output df_probs as a dictionary of records to be returned as JSON
-        probs = df_probs.to_dict('records') 
-        return(GPOutput(probs=probs) )
+        df_sim_go, df_sim_dis, weights_go, weights_dis = gp.make_sim_dfs()
+        df_edgelist, isolated_genes, df_edge_sym, isolated_genes_sym = gp.make_small_edgelist()
+        df_convert_out, positive_genes = gp.alter_validation_df()
+
+        return(GPOutput(probs=df_probs.to_dict('records'), 
+                        sim_go = df_sim_go.to_dict('records'), 
+                        edge_list=df_edgelist.to_dict('records'),
+                        # sim_dis, 
+                        # avgps, 
+                        # df_convert_out, 
+                        # positive_genes,
+                        # graph = make_graph(df_edgelist, df_probs)
+                        )
+        )
 
 
+######### api
 from fastapi import FastAPI
 
 app = FastAPI()
 
 # instantiate class to run the GP pipeline
-gprunner = GPRunner(os.getenv('FILE_LOC'))
+# doing this here limits the network to one type for the entire run
+net_type = os.getenv('NETWORK') or 'BioGRID'
+file_loc = os.getenv('FILE_LOC') or '../.data'
+
+gprunner = GPRunner(file_loc, net_type)
 
 @app.get("/")
 async def root():
@@ -118,3 +155,43 @@ async def run(gpinput: GPInput) -> GPOutput:
    
 
 
+#### saved but unused
+#TODO remove this step from here, and instead rename columns for edgelist structure
+
+class GPgraphlink(BaseModel):
+    source: int
+    target: int
+    weight: int
+    
+class GPgraphnode(BaseModel):
+    """this is the same as probs with columns renamed for D3 viz library.  It has some unfortunate element 
+    names that are python reserved words.  """
+    id_: int = Field(alias="id")
+    class_ : str = Field(alias="class")
+    Symbol: str
+    Name: str
+    Probability: float
+    Known_Novel: str = Field(alias="Known/Novel") 
+    Rank: int
+
+
+class GPgraph(BaseModel):
+    nodes: list[GPgraphnode]
+    links: list[GPgraphlink]
+
+def make_graph(df_edgelist, df_probs, max_num_genes:int = 50) -> GPgraph:
+    """create data structure with names easier for D3 network viz"""
+    # needed for D3 inside Javascript from edgelist and probs outputs
+    edges = df_edgelist.copy().fillna(0)
+    
+    # rename colums of edges and nodes for D3
+    edges.columns = ['source', 'target', 'weight']
+    nodes = df_probs[0:max_num_genes].copy()
+    nodes = nodes.rename(columns={'Entrez': 'id', 'Class-Label': 'Class'})
+    nodes = nodes.astype({'id': int})
+    
+    graph = {"nodes": nodes.to_dict(orient='records'), 
+                "links": df_edgelist.to_dict(orient='records')
+            }
+    
+    return graph
