2fd6c793b1fdc695dae8c902f36e372975d141da
Pat Bills <billspat@msu.edu>
2023-09-01 16:10:09 -0400

    shorter network-specific dockerfile tag

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,7 +2,7 @@
 
 for NET in `python -c "import geneplexus; print(' '.join(geneplexus.config.ALL_NETWORKS))"`
 do
-    buildcmd="docker build --build-arg NETWORK_NAME=$NET -f docker/Dockerfile -t geneplexus:latest-$NET ."
+    buildcmd="docker build --build-arg NETWORK_NAME=$NET -f docker/Dockerfile -t geneplexus:$NET ."
     echo $buildcmd
     $(buildcmd)
 done
